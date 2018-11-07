2d308081d1b37d5bd97d29c5309e7fdbf6dc5404
Xercoy <corey@prak.me>
2018-11-07 01:09:11 -0800

    infrastructure/build/full_installation.sh: remove repo clone; no need

--- infrastructure/build/full_installation.sh
+++ infrastructure/build/full_installation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # grab these values as arguments, if not, set defaults
-REPO_DESTINATION=$1
-DOCKERFILE_PATH=$2
-NGINX_HOSTS_PATH=$3
+# REPO_DESTINATION=$1 
+DOCKERFILE_PATH=$2  # path to focker file
+NGINX_HOSTS_PATH=$3 # path to nginx config
+SERVER_CODE_PATH=$4 # path to .go files for server
 
 # TODO: list argument options + check for them
 
@@ -23,6 +24,11 @@ then
     NGINX_HOSTS_PATH="./nginx_superfluous_hosts_file"
 fi
 
+if [ "${SERVER_CODE_PATH}" == "" ]
+then 
+    SERVER_CODE_PATH="../../server/*.go"
+fi
+
 # install Docker
 printf "1) installing Docker...\n"
 ./ubuntu_docker_install.sh
@@ -32,8 +38,8 @@ printf "2) installing Go...\n"
 ./ubuntu_install_go.sh
 
 # clone the repo
-printf "3) cloning repo...\n"
-./ubuntu_clone_repo.sh ${REPO_DESTINATION}
+# printf "3) cloning repo...\n"
+#./ubuntu_clone_repo.sh ${REPO_DESTINATION}
 
 # install Nginx
 printf "4) installing Nginx and preparing virtual host...\n"
