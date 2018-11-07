4d717edfa2f6b828476ed2c87ce27b0ec5bb90eb
Xercoy <corey@prak.me>
2018-11-07 13:38:51 -0800

    terraform: SSH enabled, no more grabbing creds from email.

--- infrastructure/terraform/digital_ocean_droplet.tf
+++ infrastructure/terraform/digital_ocean_droplet.tf
@@ -1,10 +1,16 @@
 # Set the variable value in *.tfvars file
 # or using -var="do_token=..." CLI option
 variable "do_token" {}
+variable "do_ssh_key_path" {}
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
   token = "${var.do_token}"
+}
+
+resource "digitalocean_ssh_key" "default" {
+  name       = "Terraform Example"
+  public_key = "${var.do_ssh_key_path}"
 }
 
 # Create a web server
@@ -13,4 +19,5 @@ resource "digitalocean_droplet" "web" {
     name = "superfluous"
     region = "nyc3"
     size = "s-1vcpu-1gb"
+    ssh_keys = ["${digitalocean_ssh_key.default.fingerprint}"]
 }
